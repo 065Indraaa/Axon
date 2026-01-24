848176444f195cc4d545d38936d143e131a39ffe
065Indraaa <indrbchtr06@gmail.com>
2026-01-25 04:39:06 +0700

    AA

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -9,6 +9,7 @@ export const config = createConfig({
       appName: 'AXON', // Match the name in OnchainKitProvider
       preference: 'all',
       appLogoUrl: 'https://axon.finance/logo.png',
+      projectId: import.meta.env.VITE_CDP_PROJECT_ID,
     }),
   ],
   ssr: false,
