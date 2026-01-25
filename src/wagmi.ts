2258325a4abdf646b92b1ffac602d6c3a8923db0
065Indraaa <indrbchtr06@gmail.com>
2026-01-25 10:56:31 +0700

    aa

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -7,7 +7,7 @@ export const config = createConfig({
     connectors: [
         coinbaseWallet({
             appName: 'AXON',
-            preference: 'all',
+            preference: 'smartWalletOnly',
             version: '4',
             projectId: import.meta.env.VITE_CDP_PROJECT_ID,
         } as any),
